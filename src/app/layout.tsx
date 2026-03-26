a5be45e3f592aa80aacc31b68f663b75b5e689f4
Aaqeb11 <aaqebahmed@gmail.com>
2026-03-26 17:48:34 +0300

    fix: add missing component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} overflow-hidden`}
         >
           {children}
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
